446af7cac9e2e61576dada41d3899d3404a02a4c
Calista Nguyen <calistanguyen@Calistas-MacBook-Pro.local>
2021-12-29 00:48:33 -0700

    adding pop up dialog

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:noted/note_card.dart';
 import 'note.dart' as note;
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,11 +33,18 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color.fromRGBO(255, 238, 255, 1),
         fontFamily: 'Dongle',
         textTheme: const TextTheme(
-          headline1: TextStyle(
-              fontSize: 64, fontWeight: FontWeight.w700, color: Colors.white),
-          bodyText1: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.w400, color: Colors.black),
-        ),
+            headline1: TextStyle(
+                fontSize: 64, fontWeight: FontWeight.w700, color: Colors.white),
+            headline2: TextStyle(
+                fontSize: 48, fontWeight: FontWeight.w700, color: Colors.black),
+            headline3: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.w700, color: Colors.black),
+            bodyText1: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.w400, color: Colors.black),
+            bodyText2: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w100,
+                color: Colors.black)),
       ),
       home: const MyHomePage(title: 'noted'),
     );
@@ -78,11 +86,108 @@ class _MyHomePageState extends State<MyHomePage> {
                     text: noteList[index].text, date: noteList[index].date);
               })),
       floatingActionButton: FloatingActionButton(
-          onPressed: _addToList,
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => AddNoteDialog());
+          },
           tooltip: 'Increment',
           child: const Icon(Icons.add),
           backgroundColor: Theme.of(context)
               .primaryColorDark), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+Widget customButton(BuildContext context) {
+  return InkWell(
+      onTap: () => {},
+      child: Center(
+          child: Container(
+              width: 180,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Theme.of(context).primaryColor),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 5.0),
+                  child: Text("submit",
+                      style: Theme.of(context).textTheme.headline3),
+                ),
+              ))));
+}
+
+String currentDate() {
+  var now = DateTime.now();
+  var formatter = DateFormat.yMd().add_jm();
+  String formattedDate = formatter.format(now);
+  return formattedDate; // 2016-01-25
+}
+
+class AddNoteDialog extends StatelessWidget {
+  const AddNoteDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+            side: BorderSide(color: Colors.black, width: 2)),
+        // title: Text('new note', style: Theme.of(context).textTheme.headline2),
+        child: SizedBox(
+          height: 320,
+          width: 333,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Text('new note',
+                    style: Theme.of(context).textTheme.headline2),
+              ),
+              SizedBox(
+                  width: 270,
+                  height: 100,
+                  child: TextField(
+                      maxLines: 8,
+                      decoration: InputDecoration(
+                        focusColor: Colors.black,
+                        hoverColor: Colors.black,
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: Colors.black, width: 1),
+                            borderRadius: BorderRadius.circular(15)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: Colors.black, width: 1),
+                            borderRadius: BorderRadius.circular(15)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                      ))),
+              Padding(
+                padding: const EdgeInsets.only(left: 35.0, top: 5),
+                child: Row(children: [
+                  Icon(Icons.calendar_today_outlined),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0, top: 5),
+                    child: Text(
+                      currentDate(),
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 35.0),
+                    child: Icon(Icons.favorite_outline),
+                  )
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: customButton(context),
+              )
+            ],
+          ),
+        ));
   }
 }
